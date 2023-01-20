2f2dfc2808547b1a19a53f16de389736d02b0536
Radug16 <117430607+Radug16@users.noreply.github.com>
2023-01-20 04:30:18 -0800

    Add files via upload

---
+++ ComputerPlayer.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class ComputerPlayer : Player
+{
+    public WaitForSeconds TurnWaitForSeconds { get; private set; }
+
+    [SerializeField]
+    private float _turnTime;
+
+    private void Start()
+    {
+        TurnWaitForSeconds = new WaitForSeconds(_turnTime);
+    }
+
+    
+    public void UpdateBehaviour(Hand humanHand)
+    {
+        int computerTotalValue = Hand.TotalValue;
+
+       
+        if (Hand.AcesCount == 0 || Hand.AcesTotalValue < 11) {
+           
+            if ((humanHand.AcesCount == 1 || humanHand.ContainsEqualOrHigher(7)) && computerTotalValue < 17) {
+                IsHitting = true;
+            }
+            
+            else if (humanHand.ContainsEqual(4, 5, 6) && computerTotalValue < 12) {
+                IsHitting = true;
+            }
+           
+            else if (humanHand.ContainsEqual(2, 3) && computerTotalValue < 13) {
+                IsHitting = true;
+            }
+            else {
+                IsHitting = false;
+            }
+        }
+       
+        else {
+            
+            if (computerTotalValue >= 19) {
+                IsHitting = false;
+            }
+           
+            else if (computerTotalValue == 18 && Hand.Count >= 3) {
+               
+                if (humanHand.AcesCount > 0 || humanHand.ContainsEqualOrHigher(9)) {
+                    IsHitting = true;
+                }
+                else {
+                    IsHitting = false;
+                }
+            }
+            else {
+                IsHitting = true;
+            }
+        }
+    }
+}
---
+++ Deck.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Random = System.Random;
+
+public class Deck
+{
+    private Random _random;
+    private Card[] _cards;
+
+    public Deck()
+    {
+        _random = new Random();
+        _cards = Resources.LoadAll<Card>("Cards");
+    }
+
+    public Card[] GetCards()
+    {
+        ShuffleCards();
+
+        return _cards;
+    }
+
+  
+    private void ShuffleCards()
+    {
+        for (int i = _cards.Length - 1; i > 0; i--) {
+            Card currentCard = _cards[i];
+            int otherCard = _random.Next(i + 1);
+
+            _cards[i] = _cards[otherCard];
+            _cards[otherCard] = currentCard;
+        }
+    }
+}
